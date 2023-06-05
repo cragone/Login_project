7664073c3aac999e3aadd156950ba90d1c36e8ca
cragone <132158708+cragone@users.noreply.github.com>
2023-06-04 21:36:44 -0400

    testing and playing with app routes and axios

--- simplelogin/app.py
+++ simplelogin/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/login_page")
 def index():
     return render_template("login_page.html")
+
+@app.route('/sign_in')
+def sign_in():
+    un = request.args.get('username',None,str)
+    return 'Hey there %s' % un  
 
 
 if __name__ == '__main__':
