f1a9b67065b4ba5fa20c07a1b47f9d28b6b1089c
cragone <132158708+cragone@users.noreply.github.com>
2023-06-06 18:49:40 -0400

    wrote app route to register page, made register page

--- simplelogin/app.py
+++ simplelogin/app.py
@@ -11,6 +11,34 @@ def sign_in():
     un = request.args.get('username',None,str)
     return 'Hey there %s' % un  
 
+@app.route("/info", methods=['POST'])
+def information():
+    username = request.arg.get("token",None,str)
+
+@app.route('/register')
+def reg_page():
+    return render_template("register.html")
+
+# @app.route('/resgistered', methods=['POST'])
+# def post_request():
+#       content = request.args.get("Username", None, str)
+#     if content is None:
+#         return "Server Error", 400
+#     connection = psycopg2.connect(
+#                     user="postgres",
+#                     password="postgrespostgres",
+#                     host="localhost",
+#                     port="5432",
+#                     database="loginbase"
+#              )
+#     cursor = connection.cursor()
+
+#     insert_query = "INSERT INTO tweets (content) VALUES (%s); COMMIT;"
+#     cursor.execute(insert_query, (content,))
+    
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
