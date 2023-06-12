bccce1dfdbd8278d4ccecfe8759812ece0b29c2d
cragone <132158708+cragone@users.noreply.github.com>
2023-06-12 14:40:21 -0400

    login to home_page, and hashed pass

--- simplelogin/app.py
+++ simplelogin/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-from functions.accounts import storeuserdata
+from functions.accounts import storeuserdata, retrieve_hashed_password
+import hashlib
 
 app = Flask(__name__)
 
@@ -16,14 +17,25 @@ mail = Mail(app)
 def index():
     return render_template("login_page.html")
 
-@app.route('/sign_in')
+@app.route('/sign_in', methods=['POST', 'GET'])
 def sign_in():
-    un = request.args.get('username',None,str)
-    return 'Hey there %s' % un  
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        stored_hashed_password = retrieve_hashed_password(username)
+        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+        if stored_hashed_password == hashed_password:
+            return render_template("home_page.html") 
+        else:
+            return 'Invalid username or password'
+    else:
+        return 'Invalid request method'
 
-@app.route("/info", methods=['POST'])
-def information():
-    username = request.args.get("token",None,str)
+
+@app.route('/home')
+def home_page():
+    return render_template('home_page.html')
+
 
 @app.route('/register', methods=['GET'])
 def reg_page():
@@ -34,16 +46,24 @@ def post_userdata():
     username = request.args.get("username",None,str)
     password = request.args.get("password",None,str)
     email = request.args.get("email",None,str)
-    print(username, password, email)
-    return storeuserdata(username, password, email)
+    hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    
+    print(username, hashed_password, email)
+    return storeuserdata(username, hashed_password, email)
+    
 
-@app.route('/send_email', methods=['GET','POST'])
+
+@app.route('/send_email', methods=['GET', 'POST'])
 def send_email():
-      if request.method == 'POST':
-          msg = Message("Verify HERE", sender='noreply@runclub.com', recipients=['email'])
-          msg.body = "Welcome to RUNCLUB, please verify email here"
-          mail.send(msg)
-          return "sent email."  
+    if request.method == 'POST':
+        email = request.form.get('email')  
+        msg = Message("Verify HERE", sender='noreply@runclub.com', recipients=[email])
+        msg.body = "Welcome to RUNCLUB, please verify your email here"
+        mail.send(msg)
+        return "Email sent successfully."
+    else:
+        return "Invalid request."
+
 
     
 
